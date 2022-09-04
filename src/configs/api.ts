36ea6e202773920f45c3eb6554378b045dfc1fae
Sa888nO <shura.reno.97@bk.ru>
2022-09-04 20:19:12 +0300

    more keys

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -9,4 +9,8 @@ export const KEYS = {
   key3: "4bfa0d979ed848efae461abcc8f8f87f",
   key4: "559c4881f76e40b8a66071f0b75c81e2",
   key5: "6c5a4d89c94e43ec92139bbb1d76c1ad",
+  key6: "6597fbe4c9f7458fbeae11348bdc6c4f",
+  key7: "b331801b6a864213934783d09334c0a0",
+  key8: "488017f8e73a485f9ef34b8d1db2079f",
+  key: "b852eb1f7c414f6fbf1c43938f3640d5",
 };
