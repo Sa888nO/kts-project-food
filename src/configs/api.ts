656bd2080a9d260384538ff1311a0cc7681a7e11
Sa888nO <shura.reno.97@bk.ru>
2022-09-04 15:06:34 +0300

    added new keys )))

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -6,4 +6,7 @@ export const API_ENDPOINTS = {
 export const KEYS = {
   key1: "674493274ad544d1b8cb5551c56d594f",
   key2: "335c63bdf50f48feacc1f7d817e6608c",
+  key3: "4bfa0d979ed848efae461abcc8f8f87f",
+  key4: "559c4881f76e40b8a66071f0b75c81e2",
+  key5: "6c5a4d89c94e43ec92139bbb1d76c1ad",
 };
