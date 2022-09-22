952f484f9cadc328722c3ad2f814976cb4b260e7
Sa888nO <shura.reno.97@bk.ru>
2022-09-22 15:03:48 +0300

    fix styles card and delete button

--- src/components/Card/components/InfoBlock/InfoBlock.tsx
+++ src/components/Card/components/InfoBlock/InfoBlock.tsx
@@ -27,39 +27,6 @@ const InfoBlock: React.FC<InfoBlockProps> = ({
       </div>
       <div className={`${styles["footer"]}`}>
         <div className={`${styles["footer_count-kcal"]}`}>{countKcal} kcal</div>
-        <button
-          type="button"
-          className={`${styles["footer_button"]}`}
-          onClick={() => onClickForButton}
-        >
-          <svg
-            width="50"
-            height="50"
-            viewBox="0 0 24 24"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-          >
-            <circle cx="12" cy="12" r="12" fill="#FF0000" />
-            <line
-              x1="11.8929"
-              y1="6.75"
-              x2="11.8929"
-              y2="17.25"
-              stroke="white"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-            />
-            <line
-              x1="6.75"
-              y1="12.1071"
-              x2="17.25"
-              y2="12.1071"
-              stroke="white"
-              strokeWidth="1.5"
-              strokeLinecap="round"
-            />
-          </svg>
-        </button>
       </div>
     </>
   );
